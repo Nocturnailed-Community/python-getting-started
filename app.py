9a17e2edc4fcc22dbb981017f06672b3b8f04b5b
Muhammad Ikhwan Fathulloh <77288014+Muhammad-Ikhwan-Fathulloh@users.noreply.github.com>
2023-05-28 23:32:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
-import os
-import pydevd_pycharm
-from flask import Flask
+# A very simple Flask Hello World app for you to get started with...
+
+#from flask import Flask
+import numpy as np
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/')
 def hello_world():
-    msg = 'Hello World!'
-    return msg
+    return 'K-Means Clustering Society Status!'
 
-def attach():
-  if os.environ.get('WERKZEUG_RUN_MAIN'):
-    print('Connecting to debugger...')
-    pydevd_pycharm.settrace('0.0.0.0', port=9000, stdoutToServer=True, stderrToServer=True)
+@app.route('/api/predict',methods=['POST'])
+def predict():
+    #open file
+    file = open("modelkmeans.sav","rb")
+    #load trained model
+    trained_model = joblib.load(file)
+    
+    # Get the data from the POST request.
+    datas = request.get_json(force=True)
 
-if __name__ == '__main__':
-  print('Starting hello-world server...')
-  # comment out to use Pycharm's remote debugger
-  # attach()
+    pred = []
 
-  app.run(host='0.0.0.0', port=8080)
+    # Make prediction using model loaded from disk as per the data.
+    for data in datas:
+        prediction = trained_model.predict([np.array([data["jumlah_anak"],data["penghasilan"]])])
+
+        # Take the first value of prediction
+        output = prediction[0]
+        out = 'Tidak Layak' if output==1 else 'Layak'
+        pred.append(out)
+    return jsonify(pred)
